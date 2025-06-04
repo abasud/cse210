4c6db2e166c6adcf405dab5af91a0418cf1d7bef
abasud <doctorbermeo@gmail.com>
2025-06-04 11:43:23 -0500

    Week 5 Inheritance Homework

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Console.WriteLine();
+        MathAssignment math = new MathAssignment("Matthew Jonas", "Factorization", "Section 7.3", "Problems 8-19");
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+        Console.WriteLine();
+        WritingAssignment writing = new WritingAssignment("Matthew Jonas", "America Colonization", "The Cultural Impact of the Incursion of Spain in South America");
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }
