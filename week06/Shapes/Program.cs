34764c9643519ef1c7313d2718d86cf59ccee834
abasud <doctorbermeo@gmail.com>
2025-06-11 12:31:26 -0500

    Shapes Assignment

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square shape1 = new Square(8, "Blue");
+        shapes.Add(shape1);
+
+        Rectangle shape2 = new Rectangle(5, 4, "Yellow");
+        shapes.Add(shape2);
+
+        Circle shape3 = new Circle(7, "Green");
+        shapes.Add(shape3);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            Console.WriteLine($"The {color} shape has an area of {area:F2}.");
+        }
     }
 }
